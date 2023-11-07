a58cb50a871c7783b63fbff221633b4d2405cbe6
pedroowb <credential.helper>
2023-11-07 10:10:26 -0300

    atualização com titulo

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 import random
+import time
 # Projeto solicitado pelo Felipão da DIO.io, basicamente era pra fazer um sistema de classes que exibisse X informações relacionadas aquela classe
+# Tem alguns detalhes, que não me atentei enquanto criava o projeto, que eram algumas boas práticas de PYTHON em específico, por exemplo, o estilo camel case não é utilizado em Python
+# Mas como eu tenho uma familiaridade com o tipo camel case, não me atentei em usar o snake case, e também não me atentei em usar o padrão de classes e variáveis com nomes em inglês por boas praticas..
 # Porém, decidi usar pseudo-aleatoriedade para fazer essa atividade de uma forma mais dinâmica e para ampliar os conhecimentos:
 class Jogador: # Ainda não dei uma funcionalidade pra esse classe.. mas logo mais ela tera, usando a função de randomizar nome e classe
     def __init__(self, nome, classe):
@@ -35,8 +38,7 @@ class Equipamento: # A classe mais importante do projeto, contendo dentro dela a
         print(f'Seu armamento é um(a) {self.arma}, e seu dano atualmente é de {self.dano} AD/AP')
 
 def AleatorizarNome(): # Uma simples função que contém uma variável armazenando uma lista, que é embaralhada e tem um resultado selecionado, algoritmo simples e eficaz, graças a biblioteca "Random"
-    listaNome = ['Tolric', 'HewFrith', 'Badic', 'Marmafrea', 'Egar', 'Thascas',
-             'Isenhal', 'Drax', 'Richethel', 'Bryt', 'Pabrand', 'Wenledryt']
+    listaNome = ['Tolric', 'HewFrith', 'Badic', 'Marmafrea', 'Egar', 'Thascas','Isenhal', 'Drax', 'Richethel', 'Bryt', 'Pabrand', 'Wenledryt']
     randomiName = random.choice(listaNome)
     return randomiName
 
@@ -45,11 +47,20 @@ def AleatorizarClasse(): # A mesma ideia, porém com classes ao invés de nomes,
     randomiClasse = random.choice(listaClasse)
     return randomiClasse
 
+def Titulo(): # Função para criar um título mais agradável de se visualizar no prompt
+    maintitle = print('=' * 50) 
+    title = 'GERADOR DE HEROI RPG'
+    print(f'{title:=^50}'.format(title))
+    print('=' * 50)
+    return maintitle
+
 def main(): # Função principal, responsável por ser o epicentro de todo o código, basicamente reunindo tudo para compilação final
+    maintitle = Titulo()
     randomizarClasse = AleatorizarClasse()
     print(randomizarClasse)
     armamento = Equipamento(randomizarClasse)
     armamento.armamento()
     armamento.listarArmamento()
+    
 
 main() # Chamada da função principal
